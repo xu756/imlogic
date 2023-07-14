827eaf00e5064ff478a190bc0596c18e2d090a44
xu756 <756334744@qq.com>
2023-07-15 01:03:23 +0800

    配置

--- backend/api/public/internal/config/config.go
+++ backend/api/public/internal/config/config.go
@@ -1,7 +1,13 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	rest.RestConf
+	Public    zrpc.RpcClientConf
+	RedisConf redis.RedisConf
 }
