d17777e595bc3c59d5851edfffb708eba6786544
xu756 <756334744@qq.com>
2023-12-06 16:22:59 +0800

    测试

--- cmd/im/handler/read.go
+++ cmd/im/handler/read.go
@@ -1,6 +1,8 @@
 package handler
 
-import "github.com/xu756/imlogic/internal/tool"
+import (
+	"github.com/xu756/imlogic/internal/tool"
+)
 
 func (c *Client) logic(msg *Message) {
 	ClientManager.broadcast <- &Message{
