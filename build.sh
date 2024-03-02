22d2cb5dc1077e0376938280844ceb0e75386c10
xu756 <756334744@qq.com>
2024-03-02 13:11:44 +0800

    add Dockerfile

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# 检查环境变量CODING_DOCKER_IMAGE_NAME是否已设置
-if [ -z "${CODING_DOCKER_IMAGE_NAME}" ]; then
-  echo "CODING_DOCKER_IMAGE_NAME环境变量未设置。"
+# 检查环境变量DOCKER_IMAGE_NAME是否已设置
+if [ -z "${DOCKER_IMAGE_NAME}" ]; then
+  echo "DOCKER_IMAGE_NAME环境变量未设置。"
   exit 1
 fi
 
-# 根据CODING_DOCKER_IMAGE_NAME设置mapped_value
-if [ "${CODING_DOCKER_IMAGE_NAME}" == "user-rpc" ]; then
+# 根据DOCKER_IMAGE_NAME设置mapped_value
+if [ "${DOCKER_IMAGE_NAME}" == "user-rpc" ]; then
   mapped_value="user"
-elif [ "${CODING_DOCKER_IMAGE_NAME}" == "api" ]; then
+elif [ "${DOCKER_IMAGE_NAME}" == "api" ]; then
   mapped_value="api"
-elif [ "${CODING_DOCKER_IMAGE_NAME}" == "im-rpc" ]; then
+elif [ "${DOCKER_IMAGE_NAME}" == "im-rpc" ]; then
   mapped_value="im/rpc"
-elif [ "${CODING_DOCKER_IMAGE_NAME}" == "im-server" ]; then
+elif [ "${DOCKER_IMAGE_NAME}" == "im-server" ]; then
   mapped_value="im/server"
 else
-  echo "没有找到${CODING_DOCKER_IMAGE_NAME}的映射值。"
+  echo "没有找到${DOCKER_IMAGE_NAME}的映射值。"
   exit 2
 fi
 # 如果没有找到映射值，退出脚本
 if [ -z "${mapped_value}" ]; then
-  echo "没有找到${CODING_DOCKER_IMAGE_NAME}的映射值。"
+  echo "没有找到${DOCKER_IMAGE_NAME}的映射值。"
   exit 2
 fi
 
