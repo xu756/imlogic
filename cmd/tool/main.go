9c26c0f6d7b419b2cf55e86da1e8d3477301fe99
xu756 <756334744@qq.com>
2024-03-06 12:17:50 +0800

    ping tool

--- cmd/tool/main.go
+++ cmd/tool/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/go-ping/ping"
 	"github.com/hertz-contrib/websocket"
 )
 
@@ -33,4 +34,44 @@ func main() {
 		})
 	})
 	h.Spin()
+}
+
+func AddrPing(Addr string, ws *websocket.Conn) {
+	defer ws.Close()
+	pinger := ping.New(Addr)
+	pinger.Count = 3
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		err := ws.WriteJSON(pkt)
+		if err != nil {
+			return
+		}
+	}
+
+	pinger.OnDuplicateRecv = func(pkt *ping.Packet) {
+		err := ws.WriteJSON(pkt)
+		if err != nil {
+			return
+		}
+	}
+
+	pinger.OnFinish = func(stats *ping.Statistics) {
+		err := ws.WriteJSON(stats)
+		if err != nil {
+			return
+		}
+	}
+	pinger.OnSetup = func() {
+		err := ws.WriteJSON("ping setup   " + pinger.Addr())
+		if err != nil {
+			return
+		}
+	}
+	err := pinger.Run()
+	if err != nil {
+		err := ws.WriteJSON(err.Error())
+		if err != nil {
+			return
+		}
+	}
+
 }
