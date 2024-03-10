cea67894cbfc6cb648455c9537793dfa377a1a47
xu756 <756334744@qq.com>
2024-03-10 13:22:42 +0800

    img

--- common/influxdb/msg.go
+++ common/influxdb/msg.go
@@ -4,26 +4,36 @@ import (
 	"context"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/xu756/imlogic/kitex_gen/im"
+	"log"
 	"time"
 )
 
 type MsgApi interface {
 	CreateMsg(ctx context.Context, msg *im.Message) (err error)
-	//QueryMsg(ctx context.Context, from, to string) (res *im.Message, err error)
+	QueryMsg(ctx context.Context, from, to string) (res *[]im.Message, err error)
 }
 
 func (m *customClient) CreateMsg(ctx context.Context, msg *im.Message) (err error) {
 	p := influxdb2.NewPoint("msg",
-		map[string]string{"from": msg.From, "to": msg.To, "link_id": msg.From, "msg_type": msg.MsgType},
+		map[string]string{"from": msg.From, "to": msg.To, "link_id": msg.LinkId, "msg_type": msg.MsgType},
 		map[string]interface{}{"msg_id": msg.MsgId, "device": msg.Device, "msg_content": msg.MsgContent, "create_time": time.Now().Unix()},
 		time.Now())
 	return m.writeAPI.WritePoint(ctx, p)
 }
 
-//func (m *customClient) QueryMsg(ctx context.Context, from, to string) (res *im.Message, err error) {
-//	query := `from(bucket: "msg")
-//  |> range(start: -1h)
-//  |> filter(fn: (r) => r._measurement == "msg" and r._field == "from" and r._value == "from" and r._field == "to" and r._value == "to")`
-//
-//	return nil, nil
-//}
+func (m *customClient) QueryMsg(ctx context.Context, from, to string) (res *[]im.Message, err error) {
+	query := `from(bucket: "imlogic")
+	  |> range(start: -1d)
+	  |> filter(fn: (r) => r._measurement == "msg" and r.from == "` + from + `" and r.to == "` + to + `")`
+	result, err := m.queryAPI.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+	//var msgs []im.Message
+	for result.Next() {
+		log.Print(result.Record().Value())
+
+	}
+	return nil, nil
+}
